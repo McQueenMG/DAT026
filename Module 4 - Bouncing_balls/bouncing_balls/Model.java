8d96bae9971fa6c3625257d8290f48fd202242d6
Melker Gustafsson <smelker02@gmail.com>
2024-09-25 11:42:19 +0200

    Co-authored-by: p0ntu2 <p0ntu2@users.noreply.github.com> Co-authored-by: Kimshii <Kimshii@users.noreply.github.com> Co-authored-by: JacobAndersso <JacobAndersso@users.noreply.github.com>

--- Module 4 - Bouncing_balls/bouncing_balls/Model.java
+++ Module 4 - Bouncing_balls/bouncing_balls/Model.java
@@ -16,6 +16,8 @@ class Model {
 	
 	Ball [] balls;
 
+	double g = 9.82;
+
 	Model(double width, double height) {
 		areaWidth = width;
 		areaHeight = height;
@@ -31,45 +33,68 @@ class Model {
 		for (Ball b : balls) {
 
 			// detect collision with other balls
-			if(balls.length > 1) {
-				for(Ball b2 : balls) {
-					if (b2 != b){
-						double dx = b2.x - b.x;
-						double dy = b2.y - b.y;
-						double distance = Math.sqrt(dx * dx + dy * dy);
-						if (distance < b.radius + b2.radius) {
-							double tmp = b.vx;
-							b.vx = b2.vx;
-							b2.vx = tmp;
-							tmp = b.vy;
-							b.vy = b2.vy;
-							b2.vy = tmp;
-							
-						}
-					}
-				}
+			for(Ball b2 : balls) {
+				if (b2 == b) continue;
+
+				if(!checkBallCollision(b, b2)) continue;
+
+				double u1 = Math.sqrt(b.vx*b.vx + b.vy*b.vy);
+				double u2 = Math.sqrt(b2.vx*b2.vx + b2.vy*b2.vy);
+
+				double I = b.m * u1 + b2.m * u2;
+				double R = u2 - u1;
+				
+				double v1 = (I + (b2.m * R)) / (b.m + b2.m) ;
+				double v2 = v1 - R;
+
+				b.vx = v1/2;
+				b2.vx = v2/2;
+
+				b.vy = v1/2;
+				b2.vy = v2/2;
 			}
 
 			// detect collision with the border
 			if (b.x < b.radius || b.x > areaWidth - b.radius) {
 				b.vx *= -1; // change direction of ball
 			}
+
 			if (b.y < b.radius || b.y > areaHeight - b.radius) {
 				b.vy *= -1;
 			}
 			
+			// apply gravity
+			b.vy -= g * deltaT;
 
-			// compute new position according to the speed of the ball
+ 			//Change velocity according to the speed of the ball
 			b.x += deltaT * b.vx;
 			b.y += deltaT * b.vy;
-			b.vy += deltaT * -9.82;
 		}
 	}
+
+	private void rectToPolar(Ball ball) {
+		double fi = Math.acos(ball.x / Math.sqrt(ball.x * ball.x + ball.y * ball.y));
+		ball.x = Math.cos(fi);
+		ball.y = Math.sin(fi);
+
+		// cos(x) = närliggande / hypotenusa
+		// x = arccos(närliggande / hypotenusa)
+	}
+
+	private boolean checkBallCollision(Ball b, Ball b2)
+	{
+		double dx = b2.x - b.x;
+		double dy = b2.y - b.y;
+		double distance = Math.sqrt(dx * dx + dy * dy);
+		return distance < b.radius + b2.radius;
+	}
+
 	
 	/**
 	 * Simple inner class describing balls.
 	 */
 	class Ball {
+
 		
 		Ball(double x, double y, double vx, double vy, double r) {
 			this.x = x;
@@ -77,11 +102,12 @@ class Model {
 			this.vx = vx;
 			this.vy = vy;
 			this.radius = r;
+			this.m = r*r*Math.PI; //Fake mass that is proportional to the area of the ball
 		}
 
 		/**
 		 * Position, speed, and radius of the ball. You may wish to add other attributes.
 		 */
-		double x, y, vx, vy, radius;
+		double x, y, vx, vy, radius, m;
 	}
 }
